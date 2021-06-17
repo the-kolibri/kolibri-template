f366b8cbfe32fea043fb26164c539a6cd3cc6212
Kolibri <dev@kolibri.mx>
2021-06-17 10:33:04 -0500

    Add new base-issues script version resolve #12 resolve #14 resolve #15 resolve #16

--- .konfig/basic-issues.sh
+++ .konfig/basic-issues.sh
@@ -1,18 +1,21 @@
 #!/usr/bin/env bash
 
-# Kolibri's repository configuration
+# Kolibri's base repository configuration
+# v1.0.0
 
 G_U="the-kolibri" # GITHUB_USER
 L_O="⚙️ Settings" # LABEL_ONE
-L_T="📦 Release Name v0.0.0" # LABEL_TWO
+L_T="📦 Octopus" # LABEL_TWO
 MS="Base repo files" # MILESTONE
+PN="GitHub Cli Lab" # PROJECT NAME
 
 CoreIssues() {
-    gh issue create -t 'Add README file' -b 'Custom README file for the project' -l "$L_O,$L_T" -a ${G_U} -m "${MS}"
-    gh issue create -t 'Add .gitignore file' -b 'Basic .gitignore file for the project' -l "$L_O,$L_T" -a ${G_U} -m "${MS}"
-    gh issue create -t 'Add GitHub Issues template files' -b 'Custom GitHub Issues template files for the project' -l "$L_O,$L_T" -a ${G_U} -m "${MS}"
-    gh issue create -t 'Add GitHub PRs template files' -b 'Custom Pull Requests template files for the project' -l "$L_O,$L_T" -a ${G_U} -m "${MS}"
-    gh issue create -t 'Add License file' -b 'Project license file' -l "$L_O,$L_T" -a ${G_U} -m "${MS}"
+    gh issue create -t 'Add Konfig setup scripts' -b 'Konfig custom script for repo setup' -l "$L_O,$L_T" -a ${G_U} -m "${MS}" -p "${PN}"
+    gh issue create -t 'Add README file' -b 'Custom README file for the project' -l "$L_O,$L_T" -a ${G_U} -m "${MS}" -p "${PN}"
+    gh issue create -t 'Add .gitignore file' -b 'Basic .gitignore file for the project' -l "$L_O,$L_T" -a ${G_U} -m "${MS}" -p "${PN}"
+    gh issue create -t 'Add License file' -b 'Project license file' -l "$L_O,$L_T" -a ${G_U} -m "${MS}" -p "${PN}"
+    gh issue create -t 'Add GitHub Template files' -b 'Custom GitHub Issues and Pull Request template files for the project' -l "$L_O,$L_T" -a ${G_U} -m "${MS}" -p "${PN}"
+    gh issue create -t 'Add README cover image' -b 'Custom README cover image for the project' -l "$L_O,$L_T" -a ${G_U} -m "${MS}" -p "${PN}"
 }
 
 CoreIssues
